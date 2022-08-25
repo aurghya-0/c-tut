4475a2d0bf0b2fa1a27aa9d28732c570570ead0e
aurghya-0 <aurghya.blog@gmail.com>
2022-08-25 21:28:03 +0530

    added new files

--- returnmore.c
+++ returnmore.c
@@ -1,17 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 void func(int x, int y, int *ps, int *pd, int *pp)
 {
-        *ps = x+y;
-        *pd = x-y;
-        *pp = x*y;
+	*ps = x + y;
+	*pd = x - y;
+	*pp = x * y;
 }
-int main( )
+int main()
 {
-        int a, b, sum, diff, prod;
-        a = 6;
-        b = 4;
-        func( a, b, &sum, &diff, &prod );
-        printf("Sum = %d, Difference = %d, Product = %d\n", sum, diff, prod );
+	int a, b, sum, diff, prod;
+	a = 6;
+	b = 4;
+	func(a, b, &sum, &diff, &prod);
+	printf("Sum = %d, Difference = %d, Product = %d\n", sum, diff, prod);
 
-        return 0;
+	return 0;
 }
