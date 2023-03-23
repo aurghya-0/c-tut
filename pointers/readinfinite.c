7907a12e38ea0c0444d105935ba462a52ede1e24
Aurghyadip Kundu <53909322+aurghya-0@users.noreply.github.com>
2023-03-23 20:51:44 +0530

    Update readinfinite.c
    made the code more readable

--- pointers/readinfinite.c
+++ pointers/readinfinite.c
@@ -1,50 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void take_input(int** p, int* size)
 {
-    int *p, *q, i = 1, j, k, temp; // q for storing address of 1st number
+    int* q = *p; // q for storing the address of the first number
+    int i = 1;
     printf("Enter infinite Numbers(-1 to stop reading) :: \n");
     printf("\nEnter a[%d] :: ", i - 1);
-    p = (int *)malloc(sizeof(int)); // allocate memory for the first number
-    scanf("%d", &p[0]); // take input of the first number
+    *p = (int*)malloc(sizeof(int)); // allocate memory for the first number
+    scanf("%d", &(*p)[0]); // take input of the first number
 
-    while (p[i - 1] != -1) // read until -1 is entered
+    while ((*p)[i - 1] != -1) // read until -1 is entered
     {
         i++; // increase the array size
         // re-allocate memory to match the size of the array
-        p = (int *)realloc(p, sizeof(int) * i);
-        q = p; // store it inside q
+        *p = (int*)realloc(*p, sizeof(int) * i);
+        q = *p; // store it inside q
         printf("Enter a[%d] :: ", i);
-        scanf("%d", &p[i - 1]);
+        scanf("%d", &(*p)[i - 1]);
     }
 
-    p = q; // copy the memory of q to p
+    *p = q; // copy the memory of q to p
+    *size = i - 1; // set the size of the array
+}
+
+void bubble_sort(int* p, int size)
+{
+    int i, j, temp;
 
     // sorting numbers using bubble sort
-    // i is the total size of the array
-    for (j = 1; j < i; ++j)
+    for (i = 0; i < size - 1; ++i)
     {
-        for (k = 0; k < i - j - 1; ++k)
+        for (j = 0; j < size - i - 1; ++j)
         {
-            if (p[k] > p[k + 1])
+            if (p[j] > p[j + 1])
             {
-                temp = p[k];
-                p[k] = p[k + 1];
-                p[k + 1] = temp;
+                temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
             }
         }
     }
+}
 
-    printf("\n");
-
+void print_output(int* p, int size)
+{
+    int i;
     printf("\nAfter Sorting, Numbers are :: ");
-    for (j = 0; j < i - 1; ++j)
+    for (i = 0; i < size; ++i)
     {
-        printf(" %d", p[j]);
+        printf(" %d", p[i]);
     }
-
     printf("\n");
+}
 
+int main()
+{
+    int* p = NULL;
+    int size = 0;
+
+    take_input(&p, &size);
+    bubble_sort(p, size);
+    print_output(p, size);
+
+    free(p); // free allocated memory
     return 0;
 }
